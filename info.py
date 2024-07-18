65c99a66a614cf147b83b0543f52d47b2a332c82
kunal6397 <106019141+kunal6397@users.noreply.github.com>
2024-07-18 08:08:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -115,8 +115,8 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://structural-cecil-neonghost.koyeb.app".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://structural-cecil-neonghost.koyeb.app/".format(FQDN, PORT)
+URL = "https://average-rivi-neonghost-c19ddb03.koyeb.app/".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://average-rivi-neonghost-c19ddb03.koyeb.app/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
@@ -131,9 +131,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://structural-cecil-neonghost.koyeb.app/".format(FQDN)
+    URL = "https://average-rivi-neonghost-c19ddb03.koyeb.app/".format(FQDN)
 else:
-    URL = "https://structural-cecil-neonghost.koyeb.app/".format(FQDN)
+    URL = "https://average-rivi-neonghost-c19ddb03.koyeb.app/".format(FQDN)
 
 
 
